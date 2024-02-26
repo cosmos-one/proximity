2abd80984299e875f97ae495857fd97e890de9be
kc-1 <84696121+kelvinkaifungchan@users.noreply.github.com>
2024-02-26 20:18:24 +0000

    drag asset directly into collection

--- renderer/components/AssetViewport.tsx
+++ renderer/components/AssetViewport.tsx
@@ -1,8 +1,15 @@
+import path from "path";
 import React, { useEffect, useState } from "react";
 import { PDFViewer } from "./PDFViewer";
 import { ImageViewer } from "./ImageViewer";
 
-export const AssetViewport = ({ asset, heroImage = undefined, fileData, setDragging, fullPath }) => {
+export const AssetViewport = ({
+  asset,
+  heroImage = undefined,
+  fileData,
+  setDragging,
+  fullPath,
+}) => {
   const [type, setType] = useState("");
 
   useEffect(() => {
@@ -10,7 +17,12 @@ export const AssetViewport = ({ asset, heroImage = undefined, fileData, setDragg
   }, [fileData]);
 
   return (
-    <div className="h-full w-full" draggable onDrag={() => {setDragging(fullPath)}}>
+    <div
+      className="h-full w-full"
+      draggable
+      onDrag={() => {
+        setDragging(path.basename(fullPath));
+      }}>
       {type?.includes("pdf") ? (
         <PDFViewer pdfData={fileData} />
       ) : (
